8481552e13dcf2b55c54171e1ea791186fe902a8
AMNAALMGLY <mnmnalmagly@gmail.com>
2022-11-17 11:30:19 -0800

    add focal loss

--- src/trainer.py
+++ src/trainer.py
@@ -1,19 +1,16 @@
-
 import os
 import shutil
 import time
 from collections import defaultdict
-
 
 from torch.optim.lr_scheduler import ExponentialLR
 import torch
 import torch.nn as nn
 from tqdm import tqdm
 
-
 import wandb
 from pl_bolts import optimizers
-
+import torch.nn.functional as F
 from configs import args
 from src.utils import Metric
 
@@ -45,8 +42,8 @@ class Trainer:
         -configure optimizor:setup optim and scheduler
     """
 
-    def __init__(self, model, lr, weight_decay, loss_type, num_outputs, metric, save_dir, sched ,train_loader=None
-                 ,valid_loader=None ,test_loader=None, **kwargs):
+    def __init__(self, model, lr, weight_decay, loss_type, num_outputs, metric, save_dir, sched, train_loader=None
+                 , valid_loader=None, test_loader=None, **kwargs):
 
         '''Initializes the Trainer.
         Args
@@ -59,17 +56,16 @@ class Trainer:
         '''
         super().__init__()
 
-        self.label_name =args.label_name
+        self.label_name = args.label_name
         self.model = model
         # init fc layer
         fc_in_dim = self.model.fc.in_features
-        self.model.fc =nn.Linear(fc_in_dim ,num_outputs)
+        self.model.fc = nn.Linear(fc_in_dim, num_outputs)
         self.lr = lr
         self.weight_decay = weight_decay
         self.loss_type = loss_type
         self.save_dir = save_dir
         self.num_outputs = num_outputs
-
 
         if args.no_of_gpus > 1:
             self.model = nn.DataParallel(self.model)
@@ -89,17 +85,15 @@ class Trainer:
 
         self.setup_criterion()
 
-
-
     def _shared_step(self, batch, metric_fn):
 
-        trainloss=0
+        trainloss = 0
 
         x = torch.tensor(batch[0][:, :, :, :])
 
         if x is not None:
-        # lats = batch[2][:, :2]
-        # longs = batch[2][:, 2:]
+            # lats = batch[2][:, :2]
+            # longs = batch[2][:, 2:]
 
             label = batch[1]
             target = torch.tensor(label, )
@@ -108,7 +102,6 @@ class Trainer:
             x = x.type_as(self.typeAs.weight)
 
             # reshaping
-
 
             outputs = self.model(x)
             outputs = outputs.squeeze(dim=-1)
@@ -135,7 +128,7 @@ class Trainer:
                 fn.update(preds, target)
         else:
             print('found nan img')
-        return  trainloss
+        return trainloss
 
     def training_step(self, batch, ):
 
@@ -156,15 +149,25 @@ class Trainer:
         return loss
 
     def test_step(self, batch, ):
-        loss= self._shared_step(batch, self.metric, )
+        loss = self._shared_step(batch, self.metric, )
 
         return loss
+
+    def predict(self, batch, model):
+        if model is None:
+            model = self.model
+        outputs = model(batch)
+        outputs = outputs.squeeze(dim=-1)
+        preds = torch.sigmoid(outputs, )
+
+        preds = (preds >= 0.5).astype(torch.int32)
+
+        return preds
 
     def fit(self, trainloader, validloader, batcher_test, max_epochs, gpus, class_model=None, early_stopping=True,
             save_every=25, args=args):
 
         wandb.watch(self.model, log='all', log_freq=5)
-
 
         train_steps = len(trainloader)
 
@@ -176,7 +179,6 @@ class Trainer:
         resume_path = None
         val_list = defaultdict(lambda x: '')
         start = time.time()
-
 
         for epoch in range(max_epochs):
 
@@ -210,7 +212,6 @@ class Trainer:
                     tepoch.set_postfix(loss=train_loss.item())
                     time.sleep(0.1)
 
-
             # Metric calulation and average loss
             r2 = self.metric[0].compute()
             wandb.log({f'{self.metric_str[0]} train': r2, 'epoch': epoch})
@@ -235,9 +236,7 @@ class Trainer:
                         running_loss = valid_epoch_loss / (valid_step)
                         wandb.log({"valid_loss": running_loss, 'epoch': epoch})
 
-
                 avg_valid_loss = valid_epoch_loss / valid_steps
-
 
                 r2_valid = self.metric[0].compute()
                 self.metric[0].reset()
@@ -247,7 +246,6 @@ class Trainer:
 
                 # AGAINST ML RULES : moniter test values
                 r2_test, test_loss = self.test(batcher_test)
-
 
                 # early stopping with loss
                 if best_loss - avg_valid_loss >= 0:
@@ -291,7 +289,6 @@ class Trainer:
             self.metric[0].reset()
             self.scheduler.step()
 
-
             print("Time Elapsed for one epochs : {:.2f}m".format((time.time() - epoch_start) / 60))
 
         # choose the best model between the saved models in regard to r2 value or minimum loss
@@ -328,23 +325,22 @@ class Trainer:
 
     def test(self, batcher_test):
 
-             with torch.no_grad():
-                    test_step = 0
-                    test_epoch_loss = 0
-                    print('--------------------------Testing-------------------- ')
-                    self.model.eval()
-                    r2_test = []
-                    for record in batcher_test:
-                        test_epoch_loss += self.test_step(record).item()
-                        test_step += 1
+        with torch.no_grad():
+            test_step = 0
+            test_epoch_loss = 0
+            print('--------------------------Testing-------------------- ')
+            self.model.eval()
+            r2_test = []
+            for record in batcher_test:
+                test_epoch_loss += self.test_step(record).item()
+                test_step += 1
 
-                    for i, m in enumerate(self.metric):
-                        r2_test.append((m.compute()))
+            for i, m in enumerate(self.metric):
+                r2_test.append((m.compute()))
 
-                        wandb.log({f'{self.metric_str[i]} Test': r2_test[i], })
+                wandb.log({f'{self.metric_str[i]} Test': r2_test[i], })
 
-             return r2_test[0], (test_epoch_loss / test_step)
-
+        return r2_test[0], (test_epoch_loss / test_step)
 
     def configure_optimizers(self):
         if args.scheduler == 'cyclic':
@@ -366,7 +362,6 @@ class Trainer:
                 'step': torch.optim.lr_scheduler.StepLR(opt, step_size=5, gamma=args.lr_decay, ),
                 'cyclic': torch.optim.lr_scheduler.CyclicLR(opt, base_lr=1e-4, max_lr=self.lr, cycle_momentum=False),
 
-
             }
         }
 
@@ -379,9 +374,98 @@ class Trainer:
 
         elif self.loss_type == 'mse':
             self.criterion = nn.MSELoss()
-        elif self.loss_type=='l1':
-            self.criterion =nn.L1Loss()
+        elif self.loss_type == 'l1':
+            self.criterion = nn.L1Loss()
         elif self.loss_type == 'smoothL1':
             self.criterion = nn.SmoothL1Loss(beta=3)
+        elif self.loss_type == 'focal':
+            self.criterion = Sigmoid_Focal_Loss()
 
 
+
+# def sigmoid_focal_loss(
+#     inputs: torch.Tensor,
+#     targets: torch.Tensor,
+#     alpha: float = 0.1,
+#     gamma: float = 2,
+#     reduction: str = "none",
+# ):
+#     """
+#     Original implementation from https://github.com/facebookresearch/fvcore/blob/master/fvcore/nn/focal_loss.py .
+#     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+#
+#     Args:
+#         inputs: A float tensor of arbitrary shape.
+#                 The predictions for each example.
+#         targets: A float tensor with the same shape as inputs. Stores the binary
+#                 classification label for each element in inputs
+#                 (0 for the negative class and 1 for the positive class).
+#         alpha: (optional) Weighting factor in range (0,1) to balance
+#                 positive vs negative examples or -1 for ignore. Default = 0.25
+#         gamma: Exponent of the modulating factor (1 - p_t) to
+#                balance easy vs hard examples.
+#         reduction: 'none' | 'mean' | 'sum'
+#                  'none': No reduction will be applied to the output.
+#                  'mean': The output will be averaged.
+#                  'sum': The output will be summed.
+#     Returns:
+#         Loss tensor with the reduction option applied.
+#     """
+#
+#     p = torch.sigmoid(inputs)
+#     ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+#     p_t = p * targets + (1 - p) * (1 - targets)
+#     loss = ce_loss * ((1 - p_t) ** gamma)
+#
+#     if alpha >= 0:
+#         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+#         loss = alpha_t * loss
+#
+#     if reduction == "mean":
+#         loss = loss.mean()
+#     elif reduction == "sum":
+#         loss = loss.sum()
+#
+#     return loss
+
+
+class Sigmoid_Focal_Loss():
+    """
+    Quantile loss, i.e. a quantile of ``q=0.5`` will give half of the mean absolute error as it is calcualted as
+    Defined as ``max(q * (y-y_pred), (1-q) * (y_pred-y))``
+    """
+
+    def __init__(
+            self,
+            alpha: float = 0.1,
+            gamma: float = 2,
+            reduction: str = "mean",
+            **kwargs,
+    ):
+        """
+        Quantile loss
+        Args:
+            quantiles: quantiles for metric
+        """
+
+        super(Sigmoid_Focal_Loss, self).__init__()
+        self.alpha = alpha
+        self.gamma=gamma
+        self.reduction=reduction
+
+    def __call__(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        p = torch.sigmoid(inputs)
+        ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+        p_t = p * targets + (1 - p) * (1 - targets)
+        loss = ce_loss * ((1 - p_t) ** self.gamma)
+
+        if self.alpha >= 0:
+            alpha_t = self.alpha * targets + (1 - self.alpha) * (1 - targets)
+            loss = alpha_t * loss
+
+        if self.reduction == "mean":
+            loss = loss.mean()
+        elif self.reduction == "sum":
+            loss = loss.sum()
+
+        return loss
